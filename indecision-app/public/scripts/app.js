16f24ec106d5824a48e8023c740527bd6bbc4941
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2018-01-19 01:41:39 -0800

    Playing with basic React JSX formatting

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,1 +1,14 @@
 // app.js (indecision-app/public/scripts/app.js)
+
+console.log("app.js is running.");
+
+// JSX: JavaScript XML
+
+// var template = <p>This is JSX from app.js.</p>;
+var template = React.createElement(
+    "h1",
+    { id: "template-id" },
+    "This is JSX from app.js."
+  );
+var appRoot = document.getElementById("app");
+ReactDOM.render(template, appRoot);
